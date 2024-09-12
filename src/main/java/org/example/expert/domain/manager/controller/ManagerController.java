301372bf8bffd61e03ee875dee84e7ddcc3bd8ec
leejaehee0807 <leejaehee0807@naver.com>
2024-09-12 12:58:41 +0900

    필수기능 1-4

--- src/main/java/org/example/expert/domain/manager/controller/ManagerController.java
+++ src/main/java/org/example/expert/domain/manager/controller/ManagerController.java
@@ -38,12 +38,11 @@ public class ManagerController {
 
     @DeleteMapping("/todos/{todoId}/managers/{managerId}")
     public void deleteManager(
-            @RequestHeader("Authorization") String bearerToken,
+            @Auth AuthUser authUser,
             @PathVariable long todoId,
             @PathVariable long managerId
     ) {
-        Claims claims = jwtUtil.extractClaims(bearerToken.substring(7));
-        long userId = Long.parseLong(claims.getSubject());
-        managerService.deleteManager(userId, todoId, managerId);
+
+        managerService.deleteManager(authUser.getUserId(),userId, todoId, managerId);
     }
 }
