6b3200b2902c8da01789b0e55e076dddfed55dc2
leejaehee0807 <leejaehee0807@naver.com>
2024-09-12 13:03:17 +0900

    필수기능 2-8

--- src/main/java/org/example/expert/domain/manager/service/ManagerService.java
+++ src/main/java/org/example/expert/domain/manager/service/ManagerService.java
@@ -36,6 +36,10 @@ public class ManagerService {
         Todo todo = todoRepository.findById(todoId)
                 .orElseThrow(() -> new InvalidRequestException("Todo not found"));
 
+        if (todo.getUsers() == null){
+            throw new InvalidRequestException("Todo 사용자 정보 없음");
+        }
+
         if (!ObjectUtils.nullSafeEquals(user.getId(), todo.getUser().getId())) {
             throw new InvalidRequestException("담당자를 등록하려고 하는 유저가 일정을 만든 유저가 유효하지 않습니다.");
         }
@@ -80,6 +84,10 @@ public class ManagerService {
 
         Todo todo = todoRepository.findById(todoId)
                 .orElseThrow(() -> new InvalidRequestException("Todo not found"));
+
+        if (todo.getUser() == null){
+            throw new InvalidRequestException("Todo 사용자 정보 없음");
+        }
 
         if (todo.getUser() == null || !ObjectUtils.nullSafeEquals(user.getId(), todo.getUser().getId())) {
             throw new InvalidRequestException("해당 일정을 만든 유저가 유효하지 않습니다.");
